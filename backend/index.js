98082f4943c7510de41d5a1fe634f3f0c5b0181d
Palak Bhardwaj <palak22344@iiitd.ac.in>
2025-06-14 18:06:37 +0530

    Fix Prettier formatting issues

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
 app.listen(3000, () => console.log('Server running on port 3000'));
