2f6018f599b94521f49085f41ef90455a272b041
Palak Bhardwaj <palak22344@iiitd.ac.in>
2025-06-15 08:44:05 +0530

    Backend Changes

--- backend/index.js
+++ backend/index.js
@@ -8,4 +8,5 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error(err));
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
