25c0c669890b3c5c9531e9765221f5b83c74f0c7
Palak Bhardwaj <palak22344@iiitd.ac.in>
2025-06-22 15:09:49 +0530

    Formatting and Merging issue fix

--- backend/routes/boards.js
+++ backend/routes/boards.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Board = require('../models/Board');
+const Board = require('../models/board');
 const authMiddleware = require('../middleware/auth');
 const router = express.Router();
 
